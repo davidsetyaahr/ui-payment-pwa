f93b39bbb3a863f567c6c7953beba544efda8e59
dingga apris rahmt kurniawan <71561813+dinggasukapaus@users.noreply.github.com>
2023-03-30 13:36:31 +0700

    sementara

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
-const path = require('path')
-const nextConfig = {
+const withPWA = require('next-pwa');
+const nextConfig = withPWA({
   reactStrictMode: true,
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-  },
-}
+  pwa: {
+    dest:"public",
+    register:true,
+    skipWaiting:true,
+    disable: process.env.NODE_ENV === 'development'
+  }
+})
 
 module.exports = nextConfig
