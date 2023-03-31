af8244495bf41469bf77c8f757282a8e082b4c71
dingga apris rahmt kurniawan <71561813+dinggasukapaus@users.noreply.github.com>
2023-04-01 04:00:32 +0700

    update config for image link

--- next.config.js
+++ next.config.js
@@ -7,6 +7,19 @@ const withPWA = require('next-pwa')({
 module.exports = withPWA({
   // next.js config
   reactStrictMode: true,
+  images: {
+    domains: ['127.0.0.1'],
+  },
+  // images: {
+  //   remotePatterns: [
+  //     {
+  //       protocol: 'http',
+  //       hostname: '127.0.0.1',
+  //       port: '8000',
+  //       pathname: '/storage/',
+  //     },
+  //   ],
+  // },
   async redirects() {
     return [
       {
