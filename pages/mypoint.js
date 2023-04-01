e0baa7949340f8953e0e7699cfee9a77f3f568ab
Abiyu Candra Adianyah <42163187+Candra06@users.noreply.github.com>
2023-04-01 16:03:04 +0700

    fix[history point]  : penyesuaian attribut

--- pages/mypoint.js
+++ pages/mypoint.js
@@ -26,10 +26,10 @@ function mypoint() {
       id: tempStorage.default_student_id,
     };
     await getMyPoint({ data, perPage: perpage }, (res) => {
-      setStudentName(tempStorage.default_student_name)
+      setStudentName(tempStorage.default_student_name);
       setDataPoint(res.payload);
       setTotal(res.payload.total);
-      setname(student.default_student_name)
+      setname(student.default_student_name);
     });
   };
 
@@ -87,51 +87,48 @@ function mypoint() {
           </div>
         </div>
         <section className="section-1 bg-light p-4">
-        <div className="d-flex align-items-center justify-content-between">
-          <div>
-            <small className="font-dark fw-bold">Point:</small>
-            <div className="icon-bg rounded auto fw-500">
-            80.000
+          <div className="d-flex align-items-center justify-content-between">
+            <div>
+              <small className="font-dark fw-bold">Point:</small>
+              <div className="icon-bg rounded auto fw-500">80.000</div>
             </div>
-          <a
-            className="text-decoration-none color-blue text-end"
-            href="#"
-            target="_blank"
-          >
-          <small className="font-dark fw-bold">Nama Siswa:</small>
-          <h5 className="my-0">
-            {studentName}
-          </h5>
-          </a>
-        </div>
+            <a
+              className="text-decoration-none color-blue text-end"
+              href="#"
+              target="_blank"
+            >
+              <small className="font-dark fw-bold">Nama Siswa:</small>
+              <h5 className="my-0">{studentName}</h5>
+            </a>
+          </div>
         </section>
         <section className="section-2 mt-3 mb-last-content bg-light p-4">
           <div className="filter-date">
-          <input
-            type="date"
-            value={startDate}
-            className="start"
-            name="startDate"
-            onChange={(e) => {
-              setStartDate(e.currentTarget.value);
-            }}
-            title="start"
-          />
-          <div className="px-1"></div>
-          <input
-            type="date"
-            value={endDate}
-            name="endDate"
-            onChange={(e) => {
-              setEndDate(e.currentTarget.value);
-            }}
-            className="end"
-            title="end"
-          />
-          <div className="px-3"></div>
-          <button type="submit" onClick={filter}>
-            Filter
-          </button>
+            <input
+              type="date"
+              value={startDate}
+              className="start"
+              name="startDate"
+              onChange={(e) => {
+                setStartDate(e.currentTarget.value);
+              }}
+              title="start"
+            />
+            <div className="px-1"></div>
+            <input
+              type="date"
+              value={endDate}
+              name="endDate"
+              onChange={(e) => {
+                setEndDate(e.currentTarget.value);
+              }}
+              className="end"
+              title="end"
+            />
+            <div className="px-3"></div>
+            <button type="submit" onClick={filter}>
+              Filter
+            </button>
           </div>
 
           <table className="table table-borderless table-hover">
@@ -147,25 +144,24 @@ function mypoint() {
           </table>
           {/* {getPageNum()} */}
           <div className="d-flex justify-content-center">
-          <span className="me-2">per</span>
-          <select
-            value={perPage}
-            onChange={(e) => {
-              setPerPage(e.target.value);
-              console.log(e.target.value);
-              getData(e.target.value);
-            }}
-          >
-            {getPageNum()}
-            {/* <option value={1}>Page 1</option> */}
-          </select>
-          <span className="ms-2">page</span>
+            <span className="me-2">per</span>
+            <select
+              value={perPage}
+              onChange={(e) => {
+                setPerPage(e.target.value);
+                console.log(e.target.value);
+                getData(e.target.value);
+              }}
+            >
+              {getPageNum()}
+              {/* <option value={1}>Page 1</option> */}
+            </select>
+            <span className="ms-2">page</span>
           </div>
         </section>
         <NavBottom />
       </main>
     </div>
-
   );
 }
 
