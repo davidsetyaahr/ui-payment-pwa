7b4e8cf7cd10a3ecd642c8d22911ed01d24d1a4e
davidsetyaahr <44167895+davidsetyaahr@users.noreply.github.com>
2023-04-05 12:19:09 +0700

    Update payment_history.js

--- pages/payment_history.js
+++ pages/payment_history.js
@@ -234,8 +234,8 @@ function payment_history() {
         aria-labelledby="exampleModalLabel"
         aria-hidden="true"
       >
-        <div className="modal-dialog modal-dialog-centered modal-position-bottom">
-          <div className="modal-content animate-bottom">
+        <div className="modal-dialog modal-dialog-centered">
+          <div className="modal-content">
             <div className="modal-header border-0">
               <h1 className="modal-title fs-5" id="exampleModalLabel">
                 Payment detail:
